99a5894803ce0a709826af9f28d2d53b79f98f62
StepaskaOrig <slavic.kitaev@gmail.com>
2026-08-13 14:19:19 +0300

    commit12

--- screen.py
+++ screen.py
@@ -1,2 +1,14 @@
+import pygame
+def background_1():
+    background_colour = (0,100,0)
+    screen = pygame.display.set_mode((1220, 610))
+    pygame.display.set_caption("The Flag")
+    screen.fill(background_colour)
+    pygame.display.flip()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-
+background_1()
