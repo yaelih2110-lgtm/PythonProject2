5ac1bbfdc5eb3d2242ab76442816b1c192f1219d
StepaskaOrig <slavic.kitaev@gmail.com>
2026-08-16 12:12:22 +0300

    initial commit

--- screen.py
+++ screen.py
@@ -1,7 +1,13 @@
 import pygame
-def background_1():
+pygame.init()
+KEY_PRESSED = pygame.key.get_pressed()
+
+from PythonProject2.consts import WINDOW_HEIGHT
+
+
+def background_green():
     background_colour = (0,100,0)
-    screen = pygame.display.set_mode((1220, 610))
+    screen = pygame.display.set_mode((1250, 625))
     pygame.display.set_caption("The Flag")
     screen.fill(background_colour)
     pygame.display.flip()
@@ -11,5 +17,29 @@ def background_1():
             if event.type == pygame.QUIT:
                 running = False
 
+green1 = (1, 59, 0)
+def background_night():
+    background_colour = (0, 0, 0)
+    screen = pygame.display.set_mode((1250, 625))
+    pygame.display.set_caption("The Flag (night mode)")
+    screen.fill(background_colour)
+    for i in range(0,1250,25):
+        green_lines = pygame.Rect(i,0,1,625)
+        pygame.draw.rect(screen,green1,green_lines)
+    for j in range(0,625,25):
+        green_lines = pygame.Rect(0, j, 1250, 1)
+        pygame.draw.rect(screen, green1, green_lines)
+    pygame.display.flip()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-background_1()
+
+
+
+background_green()
+if KEY_PRESSED[pygame.K_RETURN]:
+    #print(56)
+    background_night()
